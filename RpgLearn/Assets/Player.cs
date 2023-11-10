bdd35664ae7cf10700f92f7f424c648e5f8116fd
Joeq1544 <139137968+Joeq1544@users.noreply.github.com>
2023-11-09 23:23:11 -0600

    Added Box Collider to Player and added basic movement to the Player Script.

--- RpgLearn/Assets/Player.cs
+++ RpgLearn/Assets/Player.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+    
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        //Update MoveDelta
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        moveDelta = new Vector3(x, y, 0);
+
+        //Swap Sprite Direction
+        if(moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //Move the player
+        transform.Translate(moveDelta * Time.deltaTime);
     }
-}
+} 
